3056275be11b207c5a87a2c21fcbd78b51a6e42e
hys0254 <hys0254@naver.com>
2022-11-15 01:22:30 +0900

    HYS: BOJ_1202 - solved

---
+++ week01_11_15/BOJ_1202_홍윤식.java
@@ -0,0 +1,60 @@
+package week01_11_15;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_1202_홍윤식 {
+    static int N, K;
+
+    static class Jewel implements Comparable<Jewel> {
+        int M;
+        int V;
+
+        Jewel(int M, int V) {
+            this.M = M;
+            this.V = V;
+        }
+
+        @Override
+        public int compareTo(Jewel o) {
+            if (this.M == o.M)
+                return o.V - this.V;
+            return this.M - o.M;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine());
+        long ans = 0;
+        N = Integer.parseInt(stz.nextToken());
+        K = Integer.parseInt(stz.nextToken());
+        Jewel[] jewels = new Jewel[N];
+        for (int i = 0; i < N; i++) {
+            stz = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(stz.nextToken());
+            int V = Integer.parseInt(stz.nextToken());
+            jewels[i] = new Jewel(M, V);
+        }
+        Arrays.sort(jewels);
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(bags);
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0, j = 0; i < K; i++) {
+            while (j < N && jewels[j].M <= bags[i]) {
+                pQueue.offer(jewels[j++].V);
+            }
+            if (!pQueue.isEmpty()) {
+                ans += pQueue.poll();
+            }
+        }
+        System.out.println(ans);
+    }
+}
