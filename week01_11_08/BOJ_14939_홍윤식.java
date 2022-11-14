812c36f600ab7e8ce7bd8e63b2fcc5903b1fbaf1
hys0254 <hys0254@naver.com>
2022-11-15 00:04:21 +0900

    HYS: BOJ_14939 - solved

--- week01_11_08/BOJ_14939_홍윤식.java
+++ week01_11_08/BOJ_14939_홍윤식.java
@@ -1,4 +1,3 @@
-package practice;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,42 +5,81 @@ import java.io.InputStreamReader;
 
 public class BOJ_14939_홍윤식 {
 	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = {  0, 0,-1, 1 };
-	static String[][] map = new String[10][10];
-	static int[][] binMap = new int[10][10];
+	static int[] dy = { 0, 0, -1, 1 };
+	static char[][] map = new char[10][10];
+	static char[][] tempMap = new char[10][10];
 
 	public static void main(String[] args) throws IOException {
+		int ans = Integer.MAX_VALUE;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int i = 0; i < 10; i++) {
-			String[] temp = br.readLine().split("");
-			map[i] = temp;
+
+			map[i] = br.readLine().toCharArray();
 
 		}
+		for (int i = 0; i < (1 << 10); i++) {
+			int tempAns = 0;
+			copyMap(tempMap, map);
+			tempAns = simulation(i);
+			if (isAllOff()) {
+				ans = Math.min(ans, tempAns);
+			}
+		}
+		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
 
+	}
+
+	public static boolean isAllOff() {
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				int bin =0;
-				
-				if (map[i][j].equals("O")) bin=0b10000;
-				int mask = 0b1000;
-				for (int k = 0; k < 4; k++) {
-					int nx = i + dx[k];
-					int ny = j + dy[k];
-					if (nx >= 0 && nx < 10 && ny >= 0 && ny < 10) {
-						if (map[nx][ny].equals("O")) bin=bin|mask;
-						
-					}
-					mask=mask>>1;
-				}
-			binMap[i][j]=bin;
-			System.out.printf("%05d ",Integer.parseInt(Integer.toBinaryString(binMap[i][j])) );
-			
+				if (tempMap[i][j] == 'O')
+					return false;
 			}
-			System.out.println();
-			
-			
 		}
-		
-		
+		return true;
+	}
+
+	public static int simulation(int start) {
+		int cnt = 0;
+		for (int i = 0; i < 10; i++) {
+			if ((start & (1 << i)) != 0) {
+				turnSwitches(0, i);
+				cnt++;
+			}
+		}
+		for (int i = 1; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (tempMap[i - 1][j] == 'O') {
+					turnSwitches(i, j);
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+
+	public static void turnSwitches(int posX, int posY) {
+		tempMap[posX][posY] = turnSwitch(posX, posY);
+		for (int i = 0; i < 4; i++) {
+			int nx = posX + dx[i];
+			int ny = posY + dy[i];
+			if (nx >= 0 && nx < 10 && ny >= 0 && ny < 10)
+				tempMap[nx][ny] = turnSwitch(nx, ny);
+		}
+	}
+
+	public static char turnSwitch(int posX, int posY) {
+		if (tempMap[posX][posY] == '#')
+			return 'O';
+		else
+			return '#';
+	}
+
+	public static void copyMap(char[][] target, char[][] origin) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				target[i][j] = origin[i][j];
+			}
+		}
 	}
 }
